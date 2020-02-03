e71a67f98a54ce278b05e251bea88157bc12b03c
Tiki421 <sliston21@gmail.com>
2020-02-02 22:14:32 -0600

    added-processing

--- calculator.go
+++ calculator.go
@@ -10,19 +10,59 @@ import (
 )
 
 func main() {
-	expressions := make([]string, 1)
 	for {
 		scanner := bufio.NewScanner(os.Stdin)
-		fmt.Print("gocalc>")
+		fmt.Print("goCalculator>")
 		for scanner.Scan() {
-			expressions = append(expressions, scanner.Text())
-			//res, err := processStack(expressions)
+			var expression string = scanner.Text()
+			if expression == "exit" {
+				os.Exit(0)
+			}
+			res, err := processExpression(expression)
 			if err != nil {
 				fmt.Println(err)
 			} else {
-				fmt.Println(res)
+				fmt.Println(expression + " =", res)
 			}
-			fmt.Print("gocalc>")
+			fmt.Print("goCalculator>")
 		}
 	}
+}
+
+func processExpression(e string) (float64, error) {
+	result := 0.0
+	c := strings.Split(e, " ")
+	if len(c)-1 < 2 {
+		return 0.0, errors.New("error: some arguments are not supplied")
+	}
+	num1, num2, err := parseArgs(c)
+	if err != nil {
+		return 0.0, err
+	}
+	switch c[1] {
+	case "*":
+		result = num1 * num2
+	case "/":
+		if num2 == 0.0 {
+			return 0.0, errors.New("error: you tried to divide by zero.")
+		}
+		result = num1 / num2
+	case "+":
+		result = num1 + num2
+	case "-":
+		result = num1 - num2
+	}
+	return result, nil
+}
+
+func parseArgs(c []string) (float64, float64, error) {
+	num1, err := strconv.ParseFloat(c[0], 64)
+	if err != nil {
+		return 0.0, 0.0, err
+	}
+	num2, err := strconv.ParseFloat(c[2], 64)
+	if err != nil {
+		return 0.0, 0.0, err
+	}
+	return num1, num2, nil
 }
